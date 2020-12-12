f4edb299a30794d05480b406c24583fbe24e4487
Ryo Matsunami <jane.doe3wtp4649@gmail.com>
2020-12-13 07:53:18 +0900

    add dayjs

--- news-app/src/main.js
+++ news-app/src/main.js
@@ -2,10 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router/index";
 import store from "./store/index";
-// var dayjs = require('dayjs')
+var dayjs = require('dayjs')
 
 Vue.config.productionTip = false;
-// Vue.prototype.$dayjs = dayjs
+Vue.prototype.$dayjs = dayjs
 
 new Vue({
     router,
--- news-app/src/plugins/axios.js
+++ news-app/src/plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 let baseURL = `https://newsapi.org/v2`;
-let apiKey = process.env.VUE_APP_APIKEY;
+let apiKey = process.env.VUE_APP_APIKEY || '44eb373bd1c24b68b5474741816b1551';
 const instance = axios.create({
     baseURL: baseURL,
     timeout: 30000,
